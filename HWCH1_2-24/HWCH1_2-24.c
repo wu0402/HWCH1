7dd59d977b40446c32e71347ef0dab7c46c5d1c9
wu0402 <wuyoyotaiwan@gmail.com>
2025-10-19 20:17:09 +0800

    Add files via upload
    CH1_2-24

---
+++ HWCH1_2-24/HWCH1_2-24.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    printf("叫块俱计耞案");
+    scanf("%d", &n);
+
+    if (n % 2 == 0)
+        printf("%d 琌案计\n", n);
+    else
+        printf("%d 琌计\n", n);
+
+    return 0;
+}
