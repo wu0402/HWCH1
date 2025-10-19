259501b47516b78291a4e2c05582c858f9953b2c
wu0402 <wuyoyotaiwan@gmail.com>
2025-10-19 20:10:11 +0800

    Add files via upload
    CH1_2-27

---
+++ HWCH1_2-27/HWCH1_2-27.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* 使用五個 printf 印出 */
+    printf("    *\n");
+    printf("   ***\n");
+    printf("  *****\n");
+    printf(" *******\n");
+    printf("*********\n\n");
+
+    /* 使用單一 printf 印出相同圖案 */
+    printf("    *\n   ***\n  *****\n *******\n*********\n");
+
+    return 0;
+}
